e6d700a2bba515462f6a99085c7ab03fec518b8f
Smars Hu <hubin.smars@gmail.com>
2023-05-27 01:01:34 +0800

    Update my linkedin username

--- site.config.ts
+++ site.config.ts
@@ -19,7 +19,7 @@ export default siteConfig({
   // social usernames (optional)
   // twitter: 'transitive_bs',
   github: 'smars-bin-hu',
-  linkedin: 'Smars Hu',
+  linkedin: 'smars-hu-8a1183246',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
