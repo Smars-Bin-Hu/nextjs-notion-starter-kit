71a258014623b1d830b4478403cf779ac0ba88c2
Smars Hu <hubin.smars@gmail.com>
2023-05-27 11:37:56 +0800

    config description

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'Smars Hu',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Hello World! Smars Hu is here! ',
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
