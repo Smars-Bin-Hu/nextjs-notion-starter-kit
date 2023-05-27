59772a8897a857182d5a8592ba298b5a98118e46
Smars Hu <hubin.smars@gmail.com>
2023-05-27 17:00:21 +0800

    hide the side social links

--- site.config.ts
+++ site.config.ts
@@ -18,11 +18,11 @@ export default siteConfig({
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
-  github: 'smars-bin-hu',
-  linkedin: 'smars-hu-8a1183246',
+  // github: 'smars-bin-hu',
+  // linkedin: 'smars-hu-8a1183246',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  // youtube: 'channel/UCTJWjWrajSwUSfsNyrc9acA', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
