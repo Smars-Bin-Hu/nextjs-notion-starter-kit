f3450845f9ae44814f9288f0d71f8844c2f158a0
Smars Hu <hubin.smars@gmail.com>
2024-01-27 23:49:25 -0500

    rollback

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '0ce0043b9af4462c8573078b6643fbe9',
+  rootNotionPageId: 'd168745d726c4f6ab6e2a1bf0123b9e2',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
