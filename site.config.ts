afbd55947b41217356d2eac2f29e4a8b9dce6550
Smars Hu <hubin.smars@gmail.com>
2025-02-16 03:11:18 -0500

    update youtube address

--- site.config.ts
+++ site.config.ts
@@ -22,7 +22,7 @@ export default siteConfig({
   linkedin: 'smars-hu',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
-  youtube: 'channel/@smars_hu', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  youtube: '@smars_hu', // optional youtube channel name
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
